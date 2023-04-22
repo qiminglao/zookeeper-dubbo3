14c26ba5af70480e725166b63ba38d1fb897e00c
qiminglao <qiminglao@qq.com>
2023-04-22 13:44:06 +0800

    提交

--- my-direct-consumer/src/main/java/com/ghy/www/my/direct/consumer/javaconfig/JavaConfigDubbo.java
+++ my-direct-consumer/src/main/java/com/ghy/www/my/direct/consumer/javaconfig/JavaConfigDubbo.java
@@ -7,9 +7,9 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class JavaConfigDubbo {
-    @DubboReference(interfaceName = "com.ghy.www.api.IService1", url = "dubbo://192.168.3.188:20898")
+    @DubboReference(interfaceName = "com.ghy.www.api.IService1", url = "dubbo://127.0.0.1:20898")
     private IService1 service1;
 
-    @DubboReference(interfaceName = "com.ghy.www.api.IService2", url = "dubbo://192.168.3.188:20898")
+    @DubboReference(interfaceName = "com.ghy.www.api.IService2", url = "dubbo://127.0.0.1:20898")
     private IService2 service2;
 }
--- my-token-consumer/src/main/java/com/ghy/www/my/token/consumer/javaconfig/JavaConfigDubbo.java
+++ my-token-consumer/src/main/java/com/ghy/www/my/token/consumer/javaconfig/JavaConfigDubbo.java
@@ -6,8 +6,8 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class JavaConfigDubbo {
-    // @DubboReference(interfaceName = "com.ghy.www.api.IService1", url = "dubbo://192.168.3.188:20881")
-    // private IService1 service1;
+    /*@DubboReference(interfaceName = "com.ghy.www.api.IService1", url = "dubbo://127.0.0.1:20881")
+    private IService1 service1;*/
 
     @DubboReference
     private IService1 service1;
